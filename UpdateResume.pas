a26646435f3ca2314ff8e70a34a61687bdd59f8d
Vitaly.makhaev <vitaly.makhaev@gmail.com>
2024-10-25 20:28:53 +0200

    FNC component removed

--- UpdateResume.pas
+++ UpdateResume.pas
@@ -5,7 +5,8 @@ interface
 uses
 	Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
 	Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, MemDS, DBAccess, Uni,
-	Vcl.WinXCalendars, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.Buttons, VCL.TMSFNCTypes;
+	Vcl.WinXCalendars, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.Buttons;
+//  VCL.TMSFNCTypes;
 
 type
 	TFormUpdateResume = class(TForm)
